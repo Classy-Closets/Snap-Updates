cfbcfa52c7017d364403f0836890ed59a0ea6d80
Ryan Montes <RyanMDev@users.noreply.github.com>
2020-08-07 15:38:36 -0700

    Set version to 1.2.1

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 bl_info = {
     "name": "SNaP",
     "author": "Ryan Montes",
-    "version": (1, 2, 0),
+    "version": (1, 2, 1),
     "blender": (2, 7, 8),
     "location": "Tools Shelf",
     "description": "SNaP",
