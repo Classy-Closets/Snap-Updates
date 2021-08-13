1c4acddb9444b1a3568adb1be3fd9d0b42b2a8e3
Ryan Montes <RyanMDev@users.noreply.github.com>
2021-08-13 16:28:31 -0700

    Bump version to 2.0.1

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "SNaP",
     # "author": "Ryan Montes",
-    "version": (2, 0, 0),
+    "version": (2, 0, 1),
     "blender": (2, 93, 0),
     "location": "Tools Shelf",
     "description": "SNaP",
